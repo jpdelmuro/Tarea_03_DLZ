77aecaa557902bcf5b3ede8e2a7523a6553ae105
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2025-06-20 20:06:44 -0600

    Se cambio suma a resta

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@ alert("Hola desde script.js");
 
 let a = 5;
 let b = 10;
-let suma = a + b;
+let resta = a - b;
 
-console.log("La suma de a y b es:", suma);
+console.log("La resta de a y b es:", resta);
